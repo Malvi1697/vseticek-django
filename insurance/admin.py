3f42c9742a03a36c059d0f9343c591f645b3fd73
malvi1697 <jan.vseticek@pm.me>
2024-06-25 19:51:25 +0000

    another day, another commit

--- insurance/admin.py
+++ insurance/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from insurance.models import *
 
-# Register your models here.
+
+class SubjectInsuranceAdmin(admin.ModelAdmin):
+    def full_name(self, obj):
+        return f"{obj.name} {obj.surname}"
+
+    full_name.short_description = 'Full Name'
+
+    list_display = [
+        'id',
+        'full_name',
+        'email',
+        'phone_number',
+        'adress',
+    ]
+    list_display_links = ['full_name']
+
+
+admin.site.register(InsuranceSubject, SubjectInsuranceAdmin)
--- insurance/models.py
+++ insurance/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 
+class InsuranceSubject(models.Model):
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    email = models.EmailField(blank=True)
+    adress = models.CharField(max_length=128)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+
+    class Meta:
+        verbose_name = "Insurance Subject"
+        verbose_name_plural = "Insurance Subjects"
+
+    def __str__(self):
+        return f'{self.id}: {self.name} {self.surname}'
